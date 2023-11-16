06ce3ef7b844bb7c8b1a63eca5e9bd02a742eec4
MunyinSam <kelvinsam233@gmail.com>
2023-11-16 07:22:02 +0700

    Updating File

---
+++ PythonFile/helloworld.py
@@ -0,0 +1,10 @@
+if (myInput>=80 && myInput<=100):
+    print("A")
+elif (myInput>=70 && myInput<=79):
+    print("B")
+elif (myInput>=60 && myInput<=69):
+    print("C")
+elif (myInput>=50 && myInput<=59):
+    print("D")
+elif (myInput<50):
+    print("F")
