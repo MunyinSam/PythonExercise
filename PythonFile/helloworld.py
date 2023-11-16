760a6ba4cfbf8531f7b25915ddc83a4397ba52c3
MunyinSam <kelvinsam233@gmail.com>
2023-11-16 07:52:35 +0700

    New Update

--- PythonFile/helloworld.py
+++ PythonFile/helloworld.py
@@ -8,3 +8,29 @@ elif (myInput>=50 && myInput<=59):
     print("D")
 elif (myInput<50):
     print("F")
+
+    lines = []
+
+for i in range(2):
+  myInput = input()
+  lines.append(myInput + '\n')
+  
+       
+
+A = lines[0]
+B = lines[1]
+A = int(A)
+B = int(B)
+a = lines[0]
+b = lines[1]
+divide = int(A/B)
+
+print(a + "+" + b + "= " + str(A+B))
+
+print(a + "-" + b + "= " + str(A-B))
+
+print(a + "*" + b + "= " + str(A*B))
+if (B != 0):
+  print(a + "/" + b + "= " + str(divide))
+else:
+  print("")
